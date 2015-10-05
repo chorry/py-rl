c372abcf9dbe2c39237cf7ea467b5fe5e40b3d16
Alexander Loutsenko <alutsenko@irr.ru>
2015-10-05 12:36:45 +0300

    reveal tiles around when moving

--- 001.py
+++ 001.py
@@ -8,7 +8,7 @@ SCREEN_H = 1024
 SCREEN_W = 768
 
 spritePath = '225842_hyptosis_sprites-and-tiles-for-you.png'
-SPRITE_W = SPRITE_H = 32
+TILE_W = TILE_H = 32
 
 clock = pygame.time.Clock()
 k_up = k_down = k_left = k_right = 0
@@ -16,8 +16,8 @@ direction = 0
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-TILES_ACROSS = 4 - 1
-TILES_DOWN = 4 - 1
+TILES_ACROSS = 5 - 1
+TILES_DOWN = 5 - 1
 
 PLAYER_START_X = PLAYER_START_Y = 1
 
@@ -57,12 +57,25 @@ class Map(object):
     @staticmethod
     def convertTileToCoords(coords):
         tileX, tileY = coords
-        return (tileX * SPRITE_W, tileY * SPRITE_H)
+        return (tileX * TILE_W, tileY * TILE_H)
 
     def generateRoomIfNotGenerated(self, position):
         col, row = position
         if self.map[col][row] == BLOCK_DARKNESS:
             self.map[col][row] = int (random.choice("234"))
+
+    def generateRoomsAroundCoords(self, position):
+        x,y = position
+        possiblePos = ( (x-1,y), (x+1,y), (x,y-1), (x,y+1) )
+        print possiblePos
+        for side in possiblePos:
+            x,y = side
+
+            if x >= 0 and x < len(self.map) \
+                    and y >= 0 and y < len(self.map[x]):
+                print "make a tile for %s:%s" % (x, y)
+                self.generateRoomIfNotGenerated( (x, y) )
+
 
     def isTraversable(self, position):
         x, y = position
@@ -95,7 +108,7 @@ class Game(object):
         for row in range(TILES_ACROSS + 1):
             for col in range(TILES_DOWN + 1):
                 if self.map.map[row][col] == BLOCK_DARKNESS:
-                    pygame.draw.rect(self.screen, BLACK, (row * SPRITE_W, col * SPRITE_H, SPRITE_W, SPRITE_H))
+                    pygame.draw.rect(self.screen, BLACK, (row * TILE_W, col * TILE_H, TILE_W, TILE_H))
                 if self.map.map[row][col] == BLOCK_FLOOR:
                     self.screen.blit(self.tileFloor, Map.convertTileToCoords( (row, col) ) )
                 if self.map.map[row][col] == BLOCK_TRAP:
@@ -106,7 +119,7 @@ class Game(object):
 
     def draw_PlayerTile(self):
         self.screen.blit(self.player, Map.convertTileToCoords( self.position) )
-        print "Drawing player at %s, %s" % (self.position)
+        #print "Drawing player at %s, %s" % (self.position)
         pygame.display.flip()
 
     def move(self, hor, vert):
@@ -118,7 +131,7 @@ class Game(object):
         if x > TILES_ACROSS  or x < 0 or y > TILES_DOWN  or y < 0:
             return
 
-        self.map.generateRoomIfNotGenerated( (x, y) )
+        self.map.generateRoomsAroundCoords( (x, y) )
         if self.map.isTraversable((x,y)):
             self.position = (x, y)
             self.screen.blit(self.bg, (0, 0))
@@ -146,7 +159,7 @@ class Game(object):
                         (300,300)
                     )
                     self.move(hor, vert)
-                    self.map.print_ascii_map()
+                    #self.map.print_ascii_map()
 
     def init_text(self):
         self.myfont = pygame.font.Font(None,15)
@@ -169,16 +182,16 @@ class Game(object):
 
     def loadTiles(self):
         self.spriteSheet = spritesheet(spritePath)
-        #self.player = self.spriteSheet.image_at((424, 818, SPRITE_W , SPRITE_H ), colorkey=-1)
-        self.player = self.spriteSheet.image_at((13*SPRITE_W + 8, 25*SPRITE_H+16 , SPRITE_W, SPRITE_H ),colorkey=-1)
+        #self.player = self.spriteSheet.image_at((424, 818, TILE_W , TILE_H ), colorkey=-1)
+        self.player = self.spriteSheet.image_at((13*TILE_W + 8, 25*TILE_H+16 , TILE_W, TILE_H ),colorkey=-1)
         self.player.convert_alpha()
 
-        self.bg = self.spriteSheet.image_at((0, 0, SPRITE_W , SPRITE_H ))
+        self.bg = self.spriteSheet.image_at((0, 0, TILE_W , TILE_H ))
 
-        self.tileFloor = self.spriteSheet.image_at((13*SPRITE_W+8, 22*SPRITE_H+16, SPRITE_W, SPRITE_H ))
-        self.tileWall  = self.spriteSheet.image_at((13*SPRITE_W+8, 21*SPRITE_H+16, SPRITE_W, SPRITE_H ))
+        self.tileFloor = self.spriteSheet.image_at((13*TILE_W+8, 22*TILE_H+16, TILE_W, TILE_H ))
+        self.tileWall  = self.spriteSheet.image_at((13*TILE_W+8, 21*TILE_H+16, TILE_W, TILE_H ))
 
-        self.tileTrap  = self.spriteSheet.image_at((15*SPRITE_W+8, 22*SPRITE_H+16, SPRITE_W, SPRITE_H ))
+        self.tileTrap  = self.spriteSheet.image_at((15*TILE_W+8, 22*TILE_H+16, TILE_W, TILE_H ))
 
 
 def main():
