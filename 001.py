6e87d5e1956e907f79dac24deb764cbe7fc573d5
Alexander Loutsenko <alutsenko@irr.ru>
2015-10-05 13:04:23 +0300

    use Scene class

--- 001.py
+++ 001.py
@@ -41,10 +41,118 @@ class Scene(object):
         raise NotImplementedError
 
 class DungeonScene(Scene):
-    def __init__(self):
+    def __init__(self, screen):
         super(DungeonScene, self).__init__()
+        self.screen = screen
+        #load sprites from sheet
+        self.loadTiles()
+        self.textBuffer = []
 
+        self.clock = pygame.time.Clock()
+        self.direction = 0
+        self.position = (PLAYER_START_X, PLAYER_START_Y)
+
+        self.map = Map()
+        self.map.map[PLAYER_START_X][PLAYER_START_Y] = BLOCK_FLOOR
+        self.redraw_map_tiles()
+        self.draw_PlayerTile()
+
+        self.init_text()
+
+    def redraw_map_tiles(self):
+        print self.map.map.__len__()
+        for row in range(TILES_ACROSS + 1):
+            for col in range(TILES_DOWN + 1):
+                if self.map.map[row][col] == BLOCK_DARKNESS:
+                    pygame.draw.rect(self.screen, BLACK, (row * TILE_W, col * TILE_H, TILE_W, TILE_H))
+                if self.map.map[row][col] == BLOCK_FLOOR:
+                    self.screen.blit(self.tileFloor, Map.convertTileToCoords( (row, col) ) )
+                if self.map.map[row][col] == BLOCK_TRAP:
+                    self.screen.blit(self.tileTrap, Map.convertTileToCoords( (row, col) ) )
+                if self.map.map[row][col] == BLOCK_WALL:
+                    self.screen.blit(self.tileWall, Map.convertTileToCoords( (row, col) ) )
+
+
+    def draw_PlayerTile(self):
+        self.screen.blit(self.player, Map.convertTileToCoords( self.position) )
+        #print "Drawing player at %s, %s" % (self.position)
+        pygame.display.flip()
+
+    def move(self, hor, vert):
+        x, y = self.position
+
+        x = x + hor
+        y = y + vert
+
+        if x > TILES_ACROSS  or x < 0 or y > TILES_DOWN  or y < 0:
+            return
+
+        self.map.generateRoomIfNotGenerated( (x,y) )
+        if self.map.isTraversable((x,y)):
+            self.map.generateRoomsAroundCoords( (x, y) )
+            self.position = (x, y)
+            self.screen.blit(self.bg, (0, 0))
+
+    def handle_events(self, events):
+        hor = 0
+        vert = 0
+        for event in events:
+            if not hasattr(event, 'key'): continue
+            if event.key == K_ESCAPE: sys.exit(0)
+            if event.type == pygame.KEYDOWN:
+                if event.key == K_LEFT: hor = -1
+                if event.key == K_RIGHT: hor = 1
+                if event.key == K_UP: vert = -1
+                if event.key == K_DOWN: vert = 1
+                # TODO: sync text display with player movement
+                self.move(hor, vert)
+                # self.map.print_ascii_map()
+
+    def init_text(self):
+        self.myfont = pygame.font.Font(None,15)
+
+    #TODO: add text wrapper
+    def draw_Text(self,text, coords):
+        x, y = coords
+
+        text = ""
+        for textString in self.textBuffer:
+            text += textString + ". "
+
+        label = self.myfont.render(text, 1, (255,255,255))
+
+        emptySurface =  pygame.Surface( (200,200) )
+        emptySurface.fill((0,0,0))
+        self.screen.blit(emptySurface, (x, y))
+        self.screen.blit(label, (x, y))
+        self.textBuffer = []
+
+    def loadTiles(self):
+        self.spriteSheet = spritesheet(spritePath)
+        #self.player = self.spriteSheet.image_at((424, 818, TILE_W , TILE_H ), colorkey=-1)
+        self.player = self.spriteSheet.image_at((13*TILE_W + 8, 25*TILE_H+16 , TILE_W, TILE_H ),colorkey=-1)
+        self.player.convert_alpha()
+
+        self.bg = self.spriteSheet.image_at((0, 0, TILE_W , TILE_H ))
+
+        self.tileFloor = self.spriteSheet.image_at((13*TILE_W+8, 22*TILE_H+16, TILE_W, TILE_H ))
+        self.tileWall  = self.spriteSheet.image_at((13*TILE_W+8, 21*TILE_H+16, TILE_W, TILE_H ))
+
+        self.tileTrap  = self.spriteSheet.image_at((15*TILE_W+8, 22*TILE_H+16, TILE_W, TILE_H ))
+
+    def update(self):
         pass
+
+    def render(self, screen):
+        self.draw_Text(
+            self.textBuffer,
+            (300, 300)
+        )
+
+        self.redraw_map_tiles()
+        #TRANSPARENCY IS SOMEWHERE AROUND
+        self.draw_PlayerTile()
+
 
 class Map(object):
     def __init__(self):
@@ -101,123 +209,20 @@ class Map(object):
         return True
 
 
-class Game(object):
-    def __init__(self, screen):
-        self.screen = screen
-        #load sprites from sheet
-        self.loadTiles()
-        self.textBuffer = []
-
-        self.clock = pygame.time.Clock()
-        self.direction = 0
-        self.position = (PLAYER_START_X, PLAYER_START_Y)
-
-        self.map = Map()
-        self.map.map[PLAYER_START_X][PLAYER_START_Y] = BLOCK_FLOOR
-        self.redraw_map_tiles()
-        self.draw_PlayerTile()
-
-        self.init_text()
-        self.run()
-
-    def redraw_map_tiles(self):
-        print self.map.map.__len__()
-        for row in range(TILES_ACROSS + 1):
-            for col in range(TILES_DOWN + 1):
-                if self.map.map[row][col] == BLOCK_DARKNESS:
-                    pygame.draw.rect(self.screen, BLACK, (row * TILE_W, col * TILE_H, TILE_W, TILE_H))
-                if self.map.map[row][col] == BLOCK_FLOOR:
-                    self.screen.blit(self.tileFloor, Map.convertTileToCoords( (row, col) ) )
-                if self.map.map[row][col] == BLOCK_TRAP:
-                    self.screen.blit(self.tileTrap, Map.convertTileToCoords( (row, col) ) )
-                if self.map.map[row][col] == BLOCK_WALL:
-                    self.screen.blit(self.tileWall, Map.convertTileToCoords( (row, col) ) )
-
-
-    def draw_PlayerTile(self):
-        self.screen.blit(self.player, Map.convertTileToCoords( self.position) )
-        #print "Drawing player at %s, %s" % (self.position)
-        pygame.display.flip()
-
-    def move(self, hor, vert):
-        x, y = self.position
-
-        x = x + hor
-        y = y + vert
-
-        if x > TILES_ACROSS  or x < 0 or y > TILES_DOWN  or y < 0:
-            return
-
-        self.map.generateRoomIfNotGenerated( (x,y) )
-        if self.map.isTraversable((x,y)):
-            self.map.generateRoomsAroundCoords( (x, y) )
-            self.position = (x, y)
-            self.screen.blit(self.bg, (0, 0))
-
-        self.redraw_map_tiles()
-        #TRANSPARENCY IS SOMEWHERE AROUND
-        self.draw_PlayerTile()
-
-    def run(self):
-        while 1:
-            self.clock.tick(30)
-            hor = 0
-            vert = 0
-            for event in pygame.event.get():
-                if not hasattr(event, 'key'): continue
-                if event.key == K_ESCAPE: sys.exit(0)
-                if event.type == pygame.KEYDOWN:
-                    if event.key == K_LEFT: hor = -1
-                    if event.key == K_RIGHT: hor = 1
-                    if event.key == K_UP: vert = -1
-                    if event.key == K_DOWN: vert = 1
-                    #TODO: sync text display with player movement
-                    self.draw_Text(
-                        self.textBuffer,
-                        (300,300)
-                    )
-                    self.move(hor, vert)
-                    #self.map.print_ascii_map()
-
-    def init_text(self):
-        self.myfont = pygame.font.Font(None,15)
-
-    #TODO: add text wrapper
-    def draw_Text(self,text, coords):
-        x, y = coords
-
-        text = ""
-        for textString in self.textBuffer:
-            text += textString + ". "
-
-        label = self.myfont.render(text, 1, (255,255,255))
-
-        emptySurface =  pygame.Surface( (200,200) )
-        emptySurface.fill((0,0,0))
-        self.screen.blit(emptySurface, (x, y))
-        self.screen.blit(label, (x, y))
-        self.textBuffer = []
-
-    def loadTiles(self):
-        self.spriteSheet = spritesheet(spritePath)
-        #self.player = self.spriteSheet.image_at((424, 818, TILE_W , TILE_H ), colorkey=-1)
-        self.player = self.spriteSheet.image_at((13*TILE_W + 8, 25*TILE_H+16 , TILE_W, TILE_H ),colorkey=-1)
-        self.player.convert_alpha()
-
-        self.bg = self.spriteSheet.image_at((0, 0, TILE_W , TILE_H ))
-
-        self.tileFloor = self.spriteSheet.image_at((13*TILE_W+8, 22*TILE_H+16, TILE_W, TILE_H ))
-        self.tileWall  = self.spriteSheet.image_at((13*TILE_W+8, 21*TILE_H+16, TILE_W, TILE_H ))
-
-        self.tileTrap  = self.spriteSheet.image_at((15*TILE_W+8, 22*TILE_H+16, TILE_W, TILE_H ))
-
-
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_W, SCREEN_H))
-    while 1:
-        game = Game(screen)
+    running = True
+    scene = DungeonScene(screen)
 
+    while running:
+        if pygame.event.get(QUIT):
+            running = False
+            return
 
+        scene.handle_events(pygame.event.get())
+        scene.update()
+        scene.render(screen)
+        
 if __name__ == "__main__":
     main()
